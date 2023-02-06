7ba9fef91b487722330901f159929ae8b6e09e44
ibrahimlie <elvin_66635@mail.ru>
2023-02-06 16:05:58 +0700

    step-2. Fragments, Routes, Bottom Navigation, Splash screen was added

---
+++ app/src/main/java/com/example/newsapp/ui/search/SearchFragment.kt
@@ -0,0 +1,24 @@
+package com.example.newsapp.ui.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentMainBinding
+import com.example.newsapp.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/ui/splash/SplashFragment.kt
@@ -0,0 +1,24 @@
+package com.example.newsapp.ui.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentSearchBinding
+import com.example.newsapp.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+}

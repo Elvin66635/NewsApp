85f7a9428a949c4d3e566b11da7c75b33c5d2078
ibrahimlie <elvin_66635@mail.ru>
2023-02-07 15:29:31 +0700

    step-5. Connected repository, data

--- app/src/main/java/com/example/newsapp/data/api/TestRepo.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newsapp.data.api
-
-import javax.inject.Inject
-
-class TestRepo @Inject constructor(private val newsService: NewsService) {
-    suspend fun getAll() = newsService.getHeadlines()
-}

9ab4d0eab0399a181b94ed9358a444fc11ba01ce
ibrahimlie <elvin_66635@mail.ru>
2023-02-08 18:16:37 +0700

    step-7. Details screen was added

--- app/src/main/java/com/example/newsapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/details/DetailsFragment.kt
@@ -1,17 +1,26 @@
 package com.example.newsapp.ui.details
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.newsapp.R
 import com.example.newsapp.databinding.FragmentDetailsBinding
+import java.lang.Exception
 
 class DetailsFragment : Fragment() {
 
     private var _binding: FragmentDetailsBinding? = null
     private val mBinding get() = _binding!!
+    private val bundleArgs: DetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +28,38 @@ class DetailsFragment : Fragment() {
     ): View? {
         _binding = FragmentDetailsBinding.inflate(layoutInflater, container, false)
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val articleArg = bundleArgs.article
+        articleArg.let { article ->
+            article.urlToImage.let {
+                Glide.with(this).load(article.urlToImage).into(mBinding.headerImage)
+            }
+            mBinding.headerImage.clipToOutline = true
+            mBinding.articleDetailsTitle.text = article.title
+            mBinding.articleDetailsDescriptionText.text = article.description
+            mBinding.articleDetailsButton.setOnClickListener {
+                try {
+                    Intent()
+                        .setAction(Intent.ACTION_VIEW)
+                        .addCategory(Intent.CATEGORY_BROWSABLE)
+                        .setData(Uri.parse(takeIf { URLUtil.isValidUrl(article.url) }
+                            ?.let {
+                                article.url
+                            } ?: "https://google.com"))
+                        .let {
+                            ContextCompat.startActivity(requireContext(), it, null)
+                        }
+                } catch (e: Exception) {
+                    Toast.makeText(
+                        context,
+                        "The device doesn't have any browser to view the document!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.R
 import com.example.newsapp.databinding.FragmentFavoriteBinding
@@ -36,6 +38,10 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
+        newsAdapter.setOnItemClickListener {
+            val bundle = bundleOf("article" to it)
+            view.findNavController().navigate(R.id.action_mainFragment_to_detailsFragment, bundle)
+        }
         viewModel.newsLiveData.observe(viewLifecycleOwner) { response ->
             when(response) {
                 is Resource.Success -> {

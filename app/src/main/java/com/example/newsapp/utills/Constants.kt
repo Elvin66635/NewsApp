9ff1f9dc094e85ed76691b28326f1f6413c54591
ibrahimlie <elvin_66635@mail.ru>
2023-02-15 16:13:03 +0700

    Hided API key and bugs fixed

--- app/src/main/java/com/example/newsapp/utills/Constants.kt
+++ app/src/main/java/com/example/newsapp/utills/Constants.kt
@@ -1,17 +1,16 @@
 package com.example.newsapp.utills
 
+import com.example.newsapp.BuildConfig
 import java.text.SimpleDateFormat
 import java.util.*
 
 class Constants {
     companion object {
         const val BASE_URL = "https://newsapi.org/"
-        const val API_KEY = "ac8c0c8932f44fc187bbfa5917eb1665"
+        const val API_KEY = BuildConfig.API_KEY
         const val QUERY_PAGE_SIZE = 20
         const val pattern = "dd MMMM yyyy"
         val simpleDateFormat = SimpleDateFormat(pattern)
         val date: String = simpleDateFormat.format(Date())
     }
-
-
 }
